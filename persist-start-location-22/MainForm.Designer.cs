53a8003817417dc63fa48135a764649016a4fdee
Tom Gregor <tgregor@ivsoftware.com>
2022-12-06 12:03:59 -0700

    Add project files.

---
+++ persist-start-location-22/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace persist_start_location_22
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSaveSizeAndPosition = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSaveSizeAndPosition
+            // 
+            this.buttonSaveSizeAndPosition.Location = new System.Drawing.Point(26, 60);
+            this.buttonSaveSizeAndPosition.Name = "buttonSaveSizeAndPosition";
+            this.buttonSaveSizeAndPosition.Size = new System.Drawing.Size(251, 34);
+            this.buttonSaveSizeAndPosition.TabIndex = 0;
+            this.buttonSaveSizeAndPosition.Text = "Save Size and Position";
+            this.buttonSaveSizeAndPosition.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.buttonSaveSizeAndPosition);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button buttonSaveSizeAndPosition;
+    }
+}
---
+++ persist-start-location-22/MainForm.cs
@@ -0,0 +1,35 @@
+using persist_start_location_22.Properties;
+
+namespace persist_start_location_22
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonSaveSizeAndPosition.Click += saveSizeAndPosition;
+        }
+
+        private async void saveSizeAndPosition(object? sender, EventArgs e)
+        {
+            Properties.Settings.Default.Location = Location;
+            Properties.Settings.Default.Size = Size;
+            Properties.Settings.Default.Save();
+            var textB4 = Text;
+            Text = $"Location = {Location} Size = {Size}";
+            await Task.Delay(1000);
+            Text = textB4;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (Properties.Settings.Default.Size.Equals(new Size()))
+            {
+                saveSizeAndPosition(this, EventArgs.Empty);
+            }
+            Location = Properties.Settings.Default.Location;
+            Size = Properties.Settings.Default.Size;
+        }
+    }
+}
